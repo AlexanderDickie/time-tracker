998f80c572145f91138e13a61a08e73700e46e34
alexanderdickie <bigsilvertonsky@gmail.com>
2022-11-24 23:08:45 +0000

    rendered chart mock data

---
+++ src-next/components/Chart.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+} from 'recharts';
+
+// const data = [
+//   {
+//     name: "Sun",
+//     value: 10
+//   },
+//   {
+//     name: "Mon",
+//     value: 30
+//   },
+//   {
+//     name: "Tue",
+//     value: 100
+//   },
+//   {
+//     name: "Wed",
+//     value: 30
+//   },
+//   {
+//     name: "Thu",
+//     value: 23
+//   },
+//   {
+//     name: "Fri",
+//     value: 34
+//   },
+//   {
+//     name: "Sat",
+//     value: 11
+//   }
+// ];
+//
+const Chart = ({data}) => {
+  return (
+        <LineChart width={650} height={300} data={data}>
+            <CartesianGrid vertical={false} opacity="0.2" />
+            <XAxis
+
+                />
+            <YAxis
+
+                />
+            <Line
+                dataKey="value"
+                />
+        </LineChart>
+        );
+};
+export default Chart;
--- src-next/pages/index.tsx
+++ src-next/pages/index.tsx
@@ -1,12 +1,41 @@
 import React from 'react';
 
-import AppLayout from 'components/AppLayout';
 import Home from 'containers/Home';
+import Chart from 'components/Chart';
+
+const data = [
+  {
+    name: "Sun",
+    value: 10
+  },
+  {
+    name: "Mon",
+    value: 30
+  },
+  {
+    name: "Tue",
+    value: 100
+  },
+  {
+    name: "Wed",
+    value: 30
+  },
+  {
+    name: "Thu",
+    value: 23
+  },
+  {
+    name: "Fri",
+    value: 34
+  },
+  {
+    name: "Sat",
+    value: 11
+  }
+];
 
 export default function Index() {
-  return (
-    <AppLayout>
-      <Home />
-    </AppLayout>
+  return (  
+    <Chart data = {data} />
   )
 }
